04c734f376e71bd943ddfeccc88d846a27a2b88e
Rymar Stanislav <rymar4ik@gmail.com>
2017-04-04 11:17:54 +0300

    small fix for correct redirecting

--- app/transactions/transactions.routes.ts
+++ app/transactions/transactions.routes.ts
@@ -3,10 +3,12 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {AddTransactionComponent} from "./add-transaction.component";
 import {TransactionListComponent} from "./transaction-list.component";
+import {AuthGuard} from "../_guards/auth.guard";
 
 const routes: Routes = [
 
-    { path: '', redirectTo: 'list', pathMatch: 'full' },
+    //If dont add AuthGuard service, at start you client server, first page will be Transaction, Even if user not was logined.
+    { path: '', redirectTo: 'list', pathMatch: 'full', canActivate: [AuthGuard]},
     { path: 'list', component: TransactionListComponent },
     { path: 'add', component: AddTransactionComponent }
 ];
